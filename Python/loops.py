abdf91550e97d391abe5e20d2c547cff57d6359e
seendsouza <sdammobubbles@gmail.com>
2018-10-25 10:05:38 -0400

    added recursion explanation

--- Python/loops.py
+++ Python/loops.py
@@ -4,6 +4,8 @@ A collection of loops for practice
 
 # For Loops
 
+print("For Loops")
+
 # Prints each letter in the string "banana"
 for x in "banana":
   print(x)
@@ -50,11 +52,31 @@ for x in adjective:
 
 # While Loops
 
+print("\n\nWhile Loops")
 
+true_variable = True
+
+# These are infinite loops because true_variable is always true (until you set it to be false)
+# Infinite loops are usually bad because they run forever
+# If they store data (not like this one) in memory or on drive, this will be infinitely adding data to the computer (which could lead to RAM overflow or running out of disk space)
+"""
+while true_variable == True:
+    print("I'm in an infinite loop")
+
+while true_variable:
+    print("I'm in an infinite loop again")
+"""
+
+x = -5
+while x < 0:
+    x+=1
+    print("I'm incrementing by 1 and I'm at {}".format(x))
 
 # Recursion
 
-# 
+print("\n\nRecursion")
+
+# Uses itself 
 def tri_recursion(k):
   if(k>0):
     result = k+tri_recursion(k-1)
@@ -63,5 +85,18 @@ def tri_recursion(k):
     result = 0
   return result
 
-print("\n\nRecursion Example Results")
 tri_recursion(6)
+# Explanation with 6 as an example:
+# k = 6
+# k > 0, so result is 6 + 5 + 4 + 3 + 2 + 1 + 0 = 21
+# That is the last result, but what about the previous results?
+# Remember that recursion uses itself, so everytime it calls itself, it needs to execute the whole function
+# The result is only when k is 1 because when k = 0, k !> 0 and therefore the function does not print anything, but returns the result as 0 (because of the else statement)
+# I like to think of it as going backwards from there, so result = 1 + 0 = 1 and it is printed and returned
+# Then you have the next k, which is 2, so 2 + 1 = 3
+# Then you have the next k, which is 3, so 3 + 3 = 6
+# Then you have the next k, which is 4, so 4 + 6 = 10
+# Then you have the next k, which is 5, so 5 + 10 = 15
+# Then you have the next k, which is 6, so 6 + 15 = 21
+# It prints the numbers: 1, 3, 6, 10, 15, 21 as a result
+
