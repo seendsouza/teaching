dcfed0477ca216323be71573d9a52e3f386fd8ef
seendsouza <sdammobubbles@gmail.com>
2018-10-26 23:25:26 -0400

    tbh I forgot what I did this time

--- Python/fizzbuzz/fizzbuzz.py
+++ Python/fizzbuzz/fizzbuzz.py
@@ -2,7 +2,7 @@
 Write a program that prints the numbers from 1 to 20. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."""
 def concat_strings(num):
     """
-    
+    See fizzbuzz_concat_strings.py for an explanation.
     """
     string = ""
     if num % 3 == 0:
@@ -15,7 +15,7 @@ def concat_strings(num):
 
 def if_elif_else(num):
     """
-
+    See fizzbuzz_if_elif_else.py for an explanation.
     """
     if num % 3 == 0 and num % 5 == 0:
         return('FizzBuzz')
@@ -27,10 +27,12 @@ def if_elif_else(num):
         return(num)
 def Main():
     """
+    The main function for running FizzBuzz
     """
     for num in range(1,21):
         print(concat_strings(num))
         print(if_elif_else(num))
 
+# This just means that it will only execute when you run this program. It will not run if imported into another program, but the previous functions will be improted..
 if __name__ == "__main__":
     Main()
--- Python/fizzbuzz/fizzbuzz_if_elif_else.py
+++ Python/fizzbuzz/fizzbuzz_if_elif_else.py
@@ -1,3 +1,7 @@
+# For a number from 1 to 20, if the remainder of the number divided by 3 and 5 is 0, print FizzBuzz.
+# if it's just a remainder of the number divided by 3, print Fizz
+# if it's just a remainder of the number divided by 5, print Buzz
+# if it's any other number, print the number
 for num in range(1, 21):
     if num % 3 == 0 and num % 5 == 0:
         print('FizzBuzz')
